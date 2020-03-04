7e9e1b1c7a325bcfa7c370eb23802082bc051dee
github username <vineshpersaud@gmail.com>
2020-03-04 18:16:33 -0500

    fix

--- kangaroo.py
+++ kangaroo.py
@@ -1,14 +1,13 @@
 def kangaroo(x1, v1, x2, v2):
-
+    #check if kangroo can catch first
     if x1 + v1 >  x2 + v2 and v1 > v2 :
         return('NO')
     else:
         kangOne = x1
         kangTwo = x2
-        for i in range(1000) :
-            kangOne += v1
-            kangTwo += v2
+        for i in range(10000) :
             if kangOne == kangTwo:
                 return ('YES')   
-            if i == 999:
-                return ('NO')    
+            kangOne += v1
+            kangTwo += v2
+        return ('NO')    
